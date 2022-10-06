4cba2621ed798b4e6deab40ff95f7ab03105191b
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-06 20:10:41 -0300

    criando tela modal em codigo

--- ExercicioNavegacao/ExercicioNavegacao/ViewController.swift
+++ ExercicioNavegacao/ExercicioNavegacao/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedTela01(_ sender: UIButton) {
+        performSegue(withIdentifier: "tela01", sender: nil)
+    }
 }
 
