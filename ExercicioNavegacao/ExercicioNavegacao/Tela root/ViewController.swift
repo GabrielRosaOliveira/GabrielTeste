741fd311f86a0cbe6d6b09ed85a359f2ff60bdbb
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-06 23:23:09 -0300

    coloquei comentarios

--- ExercicioNavegacao/ExercicioNavegacao/Tela root/ViewController.swift
+++ ExercicioNavegacao/ExercicioNavegacao/Tela root/ViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     
     @IBAction func tappedModalButton(_ sender: UIButton) {
+        //        exibindo tela por MODAL
         let vc = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
         present(vc ?? UIViewController(), animated: true)
     }
     
     
     @IBAction func tappedPushButton(_ sender: UIButton) {
+        //        exibindo tela por NAVIGATION
         let vc = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
         navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
     }
