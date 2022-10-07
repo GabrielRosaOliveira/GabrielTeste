e7f91c0b185c8e846235d57251ea3db8f46d0bf7
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-06 23:00:24 -0300

    criei navegacao de telas por codigo

---
+++ ExercicioNavegacao/ExercicioNavegacao/Tela root/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ExercicioNavegacao
+//
+//  Created by Gabriel on 06/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func tappedModalButton(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
+        present(vc ?? UIViewController(), animated: true)
+    }
+    
+    
+    @IBAction func tappedPushButton(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+    }
+    
+}
+
