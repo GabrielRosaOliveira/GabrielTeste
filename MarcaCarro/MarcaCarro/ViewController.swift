d3db7092afb0044a2e32b84286cade50f1e02f54
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-11-03 20:04:04 -0300

    emplementando codigo user

--- MarcaCarro/MarcaCarro/ViewController.swift
+++ MarcaCarro/MarcaCarro/ViewController.swift
@@ -40,6 +40,7 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let user = self.data[indexPath.row]
         print(data[indexPath.row].name)
     }
 }
