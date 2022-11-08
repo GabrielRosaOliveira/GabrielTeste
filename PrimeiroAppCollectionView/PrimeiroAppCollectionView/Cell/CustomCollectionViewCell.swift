01b70bd7ba25ab15ccea1c8a616ff505c9420c47
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-11-07 22:13:27 -0300

    criando app primeiro app collectionView Simples

---
+++ PrimeiroAppCollectionView/PrimeiroAppCollectionView/Cell/CustomCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomCollectionViewCell.swift
+//  PrimeiroAppCollectionView
+//
+//  Created by Gabriel on 07/11/22.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var carImageView: UIImageView!
+    
+    static let identifier: String = "CustomCollectionViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+//    ciclo de vida da celula, podendo comparar com o viewDidLoad da ViewController
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        carImageView.contentMode = .scaleAspectFill
+    }
+    
+    func setupCell(nameImage: String) {
+        carImageView.image = UIImage(named: nameImage)
+    }
+
+}
