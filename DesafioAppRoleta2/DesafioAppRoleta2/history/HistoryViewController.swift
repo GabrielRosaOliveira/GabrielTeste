bea71c167fdff834988b47cb3a538249bda8c9bc
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-12-10 15:14:36 -0300

    aplicando mock no projeto

---
+++ DesafioAppRoleta2/DesafioAppRoleta2/history/HistoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryViewController.swift
+//  DesafioAppRoleta2
+//
+//  Created by Gabriel on 08/12/22.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
